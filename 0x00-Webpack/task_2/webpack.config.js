8a2b4987085d9f0e82c7f8016b55890fa5bfe488
Val-ster <vster310@gmail.com>
2024-09-27 21:52:32 +0000

    Finished

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,54 +1,38 @@
-const path = require('path');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-	entry: './js/dashboard_main.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|jpg|jpeg|gif)$/i,
-				type: 'asset',
-				parser: {
-					dataUrlCondition: {
-						maxSize: 8192, // Convert images < 8kb to base64 strings
-					},
-				},
-				generator: {
-					filename: 'assets/[name][hash][ext][query]',
-				},
-			},
-		],
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserPlugin(),
-			new CssMinimizerPlugin(),
-			new ImageMinimizerPlugin({
-				minimizerOptions: {
-					plugins: [
-						['imagemin-mozjpeg', { quality: 65 }],
-						['imagemin-optipng', { optimizationLevel: 5 }],
-						['imagemin-svgo', {
-							plugins: [
-								{ removeViewBox: false },
-								{ cleanupIDs: false },
-							],
-						}],
-					],
-				},
-			}),
-		],
-	},
-	mode: 'production',
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
